492ae2cf44f0f61b3a8307ca260aad4ae457f9ec
Joe Stanley <engineerjoe440@yahoo.com>
2023-08-31 21:29:02 -0700

    updated with easyeffects killer

--- shutdown-recording.py
+++ shutdown-recording.py
@@ -4,10 +4,16 @@ Shut down and restore Audio PipeWire Configuration after a Recording Session.
 """
 ################################################################################
 
+import subprocess
+
 import pulsectl
+
+def stop_easyeffects():
+    subprocess.run(["killall", "easyeffects"], check=True)
 
 def main():
     """Shut down Recording System."""
+    stop_easyeffects()
     with pulsectl.Pulse('manager') as pulse:
         for stream in pulse.sink_input_list():
             if stream.name == "Playback":
