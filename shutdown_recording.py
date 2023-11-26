0cd32c5d4af26ad4b04ce03f9522205dc2633d9e
Joe Stanley <engineerjoe440@yahoo.com>
2023-11-25 21:02:48 -0800

    subtle cleanups for new system

--- shutdown_recording.py
+++ shutdown_recording.py
@@ -1,3 +1,4 @@
+#!/user/bin/python3
 ################################################################################
 """
 Shut down and restore Audio PipeWire Configuration after a Recording Session.
@@ -9,18 +10,19 @@ import subprocess
 import pulsectl
 
 def stop_easyeffects():
+    """Kill the EasyEffects Application."""
     try:
         subprocess.run(["killall", "easyeffects"], check=True)
     except subprocess.CalledProcessError:
         print("easyeffects already stopped.")
 
-def main():
+def restore_audio_levels():
     """Shut down Recording System."""
-    stop_easyeffects()
     with pulsectl.Pulse('manager') as pulse:
         for stream in pulse.sink_input_list():
             if stream.name == "Playback":
                 pulse.volume_set_all_chans(stream, 1)
 
 if __name__ == "__main__":
-    main()
+    stop_easyeffects()
+    restore_audio_levels()
